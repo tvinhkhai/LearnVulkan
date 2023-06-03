dc170efb401c0404458df9fba0e0b80cc4450c30
Tran Vinh Khai <vkhai.tran@gmail.com>
2023-06-03 22:08:32 +0700

    drawing triangle setup base code

---
+++ src/Application.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Application.h"
+
+void HelloTriangleApplication::run() {
+    initVulkan();
+    mainLoop();
+    cleanup();
+}
+
+void HelloTriangleApplication::initVulkan() {
+
+}
+
+void HelloTriangleApplication::mainLoop() {
+
+}
+
+void HelloTriangleApplication::cleanup() {
+
+}
---
+++ src/Application.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class HelloTriangleApplication {
+public:
+    void run();
+
+private:
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+};
